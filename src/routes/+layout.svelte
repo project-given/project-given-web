34e0f32ee991102fc0bfc006ce29f833fcfd3471
jameskimthing <jameskimjaewon.abc@gmail.com>
2023-10-05 12:00:14 +0900

    even MORE meta

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,19 +8,6 @@
 		'Project Given is a non-profit organization aiming to improve lives of unprivileged members with free whealthcare and education.';
 </script>
 
-<svelte:head>
-	<title>Project Given</title>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<meta name="description" content={description} />
-	<meta property="og:url" content="https://projectgiven.org/" />
-	<meta property="og:title" content="Project Given" />
-	<meta property="og:description" content={description} />
-	<meta property="og:image" content="/projectgivenfront.jpg" />
-	<meta property="og:type" content="website" />
-	<meta property="og:locale" content="en_US" />
-</svelte:head>
-
 <div class="font-poppins">
 	<!-- {#if $page.url.pathname !== '/'} -->
 	<div class="fixed top-0 z-40 w-full">
